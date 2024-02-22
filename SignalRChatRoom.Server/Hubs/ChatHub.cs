29231a56747138534a151d48562c7b87843fdb86
BetulTugce <betultugcedikdogmus@gmail.com>
2024-02-23 00:13:15 +0300

    SignalR kullanımı için temel ayarlar yapıldı.

---
+++ SignalRChatRoom.Server/Hubs/ChatHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatRoom.Server.Hubs
+{
+    // Bir sınıfın hub sınıfı olduğunu anlamak ve sorumlulukları yüklemek için hub sınıfından türetilmesi gerekir..
+    public class ChatHub : Hub
+    {
+    }
+}
--- SignalRChatRoom.Server/Program.cs
+++ SignalRChatRoom.Server/Program.cs
@@ -1,6 +1,19 @@
+using SignalRChatRoom.Server.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// CORS politikalarý ayarlarý..
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().SetIsOriginAllowed(origin => true)));
+
+// Sisteme WebSocket protokolünü kullanacaðý bildiriliyor yani SignalR modülü devreye sokuluyor..
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Ayarlanan CORS politikasýný kullanabilmek için ilgili middleware çaðýrýlýyor..
+app.UseCors();
+
+// Hubýn hangi endpointte kullanýlacaðý bildiriliyor..
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
